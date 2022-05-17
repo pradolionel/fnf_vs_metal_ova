df69aa6282005f115dd04b15bac63462d6c56bd7
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-18 02:17:48 +0400

    Add files via upload

---
+++ assets/preload/characters/rapper.lua
@@ -0,0 +1,7 @@
+function onCreate()
+
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'gameover-sonic'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+end
