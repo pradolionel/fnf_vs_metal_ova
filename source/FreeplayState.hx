dce0f8dd2535f3b2be74f7ba058b8297f578f208
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-20 16:18:19 -0500

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -89,7 +89,7 @@ class FreeplayState extends MusicBeatState
 
 		// LOAD CHARACTERS
 
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg = new FlxSprite().loadGraphic(Paths.image('unknowna'));
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
 		add(bg);
 
